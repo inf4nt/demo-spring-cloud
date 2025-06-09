bceda862b98151f2c53d9f0bd5ca03773e0910b1
Maksym Lukaretskyi <com.infant.home@gmail.com>
2025-06-09 16:53:28 +0300

    updates

--- run-k8s.sh
+++ run-k8s.sh
@@ -2,9 +2,9 @@ minikube delete
 
 minikube start
 
-docker build -t api-spring-cloud api-spring-cloud/ -f docker/Dockerfile.java21
+docker build -t api-spring-cloud api-spring-cloud/ -f docker/java21.Dockerfile
 
-docker build -t client-spring-cloud client-spring-cloud/ -f docker/Dockerfile.java21
+docker build -t client-spring-cloud client-spring-cloud/ -f docker/java21.Dockerfile
 
 minikube image load api-spring-cloud:latest
 
@@ -16,4 +16,4 @@ kubectl apply -f k8s/client-spring-cloud.configmap.yaml
 
 kubectl apply -f k8s/client-spring-cloud.deployment.service.yaml
 
-echo -e '\033[0;31mrun "minikube server --all" to open a tunnel'
+echo -e '\033[0;31mrun "minikube service --all" to open a tunnel'
